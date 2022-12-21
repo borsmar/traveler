9459ae1ccd01ad00f645357f14611904afabed64
borsmar <borsmar123@gmail.com>
2022-12-21 13:57:37 -0800

    fix for cors

--- src/main/java/com/helsystems/traveler/controller/RideController.java
+++ src/main/java/com/helsystems/traveler/controller/RideController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 @RestController
+@CrossOrigin
 public class RideController {
     @Autowired
     RideService rideService;
